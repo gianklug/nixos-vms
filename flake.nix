28b6e2de4f13d14253aadc56d2af47208202b0ec
Gian Klug <gian.klug@adfinis.com>
2026-09-13 17:12:05 +0200

    chore: 26.04

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     agenix.url = "github:ryantm/agenix";
     comin.url = "github:nlewo/comin";
   };
